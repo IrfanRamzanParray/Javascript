c80d2637f85ebd3adea81415507dc071a85e7dc2
Irfan Ramzan Parray <parrayirfan9@gmail.com>
2026-02-02 06:37:54 +0000

    03_conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -17,3 +17,24 @@ console.log(booleanIsLoggedIn);
 //1 => true   ; 2=>false;4
 //"" => false
 // "hello" => true
+
+//***********OPERATIONS****************/
+let value = 3
+let negValue = -value;
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2**2);
+
+let str1 = "irfan"
+let str2 = " ramzan"
+let str3 = str1+str2
+console.log(str3);
+
+console.log(+true);
+
+let gameCounter =100;
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
