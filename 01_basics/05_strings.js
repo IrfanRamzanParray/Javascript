6359607c8b2209595330bd34ce510b6f0bcc8e70
Irfan Ramzan Parray <parrayirfan9@gmail.com>
2026-02-03 09:19:27 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "irfan"
+const repoCount = 5;
+
+console.log(name+repoCount);
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('irfan');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
