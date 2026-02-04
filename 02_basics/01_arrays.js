d78007166f8989b1f042c43b4188aba71ef33df1
Irfan Ramzan Parray <parrayirfan9@gmail.com>
2026-02-04 10:19:43 +0000

    01_arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+//arrays
+const myArr = [0,1,2,3,4,5]
+const myHeros = ['college','schools']
+const arr = new Array(4,5,6)
+console.log(myArr[3]);
+console.log(arr.length);
+
+//Array methods
+// myArr.push(6)
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+// myArr.unshift(9);
+// console.log(myArr)
+myArr.shift()
+console.log(myArr);
