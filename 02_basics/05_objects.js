3b599b88c62034ce5d0ab7ff5c5c9e3f55e4118e
Irfan Ramzan Parray <parrayirfan9@gmail.com>
2026-02-05 07:29:36 +0000

    object de structure & json api

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,23 @@
+//OBJECT DE STRUCTURE
+const course = {
+    coursename: "javascript",
+    price: "1000",
+    courseInstructor: "chai aur code"
+}
+//course.courseInstructor
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+
+//JSON API
+
+// {
+//     "name": "irfan",
+//     "coursename": "javascript",
+//     "price": "free"
+// }
+
+[
+    
+]
