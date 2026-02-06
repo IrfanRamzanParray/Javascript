21a44ef66f944928f13f5a267e1be357d2df7f2e
Irfan Ramzan Parray <parrayirfan9@gmail.com>
2026-02-06 06:01:06 +0000

    if statement

---
+++ 04_control_flow/first.js
@@ -0,0 +1,17 @@
+//If statement
+
+if(false)
+{
+ console.log("great");
+}
+
+if(true)
+{
+ console.log("country");
+}
+
+/* comparison operators */
+// < , > , <=, >=, ==, ===, !=, !==
+const balance = 3400;
+if(balance >500) console.log("test");
+
